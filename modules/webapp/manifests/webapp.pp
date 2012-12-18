dfd719f8581aaa1c2cebddc5e73d6716c1d1772d
Yoni Yalovitsky <yoni@fewbytes.com>
2012-12-18 13:27:08 +0200

    unicorn install fixed and made simpler

--- modules/webapp/manifests/webapp.pp
+++ modules/webapp/manifests/webapp.pp
@@ -1,20 +1,8 @@
 $WEBAPP_REPO="https://github.com/redmine/redmine.git" #TODO: set from cloudify
-$WEBAPP_TAG="1.4.5"
+$WEBAPP_TAG="1.4.5" #TODO: set from cloudify
 $WEBAPP_PATH="/opt/webapps/rails"
 
 package {["rubygems", "ruby-dev", "libxml2-dev", "libxslt-dev", "libsqlite3-dev", "libmysqlclient-dev"]: }
-package {"nodejs":} #used for its js runtime engine
-
-exec {'fix gem dates':
-    command => "sed -i 's/ 00:00:00.000000000Z//g' /var/lib/gems/1.8/specifications/*",
-    path    => "/usr/bin/:/usr/local/bin/:/bin/",
-    require => Package['rubygems'],
-}
-
-package { ["rails", "unicorn", "execjs"]:
-    ensure => present,
-    provider => gem,
-}
 
 class {'apache': }
 apache::module { 'proxy_http': }
@@ -37,18 +25,17 @@ exec {'fetch webapp tag':
     require => Exec['fetch webapp repo'],
 }
 
-exec { "add mysql gems":
-    command => "echo \"gem 'mysql2'\" >>$WEBAPP_PATH/Gemfile",
-    unless  => "grep 'mysql2' $WEBAPP_PATH/Gemfile",
-    path    => "/usr/bin/:/usr/local/bin/:/bin/",
-    require => Exec['fetch webapp tag']
+#set up unicorn
+file { 'Gemfile.local':
+    path => "$WEBAPP_PATH/Gemfile.local",
+    content => "gem 'unicorn'\n",
 }
 
 exec {"bundle install":
     command => "bundle install --without development test rmagick postgresql",
     cwd     => "$WEBAPP_PATH",
     path    => "/usr/bin/:/usr/local/bin/:/bin/",
-    require => Exec['fix gem dates', "add mysql gems"]
+    require => [Exec['fetch webapp tag'], File['Gemfile.local']],
 }
 
 #a fix for: A key is required to write a cookie containing the session data. Use config.action_controller.session = { :key => "_myapp_session", :secret => "some secret phrase" } in config/environment.rb
@@ -84,12 +71,17 @@ exec {'rake tasks':
     require => File["$WEBAPP_PATH/config/database.yml"],
 }
 
+#TODO: create a redmine user and give it file system permissions?
+#mkdir tmp tmp/pdf public/plugin_assets
+#chown -R redmine:redmine files log tmp public/plugin_assets
+#chmod -R 755 files log tmp public/plugin_assets
+
 #This doesn't work well, I should move it to upstart - https://github.com/edrex/puppet-upstart
 exec {'launch unicorn':
     command => "pgrep -f unicorn -P 1 || unicorn_rails -D -E production",
     cwd     => "$WEBAPP_PATH",
     path    => "/usr/bin/:/usr/local/bin/:/bin/",
-    require => Exec['rake tasks', 'generate secret'],
+    require => [Exec['rake tasks', 'generate secret'], File['Gemfile.local']],
 }
 
 exec {'disable default vhost':
